3a922556a0b2cec5ff4b13f89a3ea3bf0c6f05ce
khancuts <khancuts@gmail.com>
2021-03-11 12:54:51 +0700

    menambahkan p lorem pada file index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum id nam dolorem totam. Cum quae at deleniti, aliquid dolorum beatae consequuntur eos quidem, voluptas hic ullam maxime autem doloremque totam?</p>
 </body>
 </html>
